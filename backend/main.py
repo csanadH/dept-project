3b0f99821e81d23af98b4ad1e8dcbdcca29687c8
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-11-22 15:22:41 +0100

    Create endpoint for retrieving movie by ID

--- backend/main.py
+++ backend/main.py
@@ -10,13 +10,17 @@ app.config.from_object(__name__)
 
 CORS(app)
 
-@app.route('/movies/<movie>', methods=['GET'])
+@app.route('/movies/name/<movie>', methods=['GET'])
 def movies(movie):
-    response_object = {'status': 'success'}
-    print(movie)
     contents = urllib.request.urlopen("http://www.omdbapi.com/?s="+movie+"&apikey=101cdcd1").read()
     contents = json.loads(contents.decode('utf-8'))
     contents = contents['Search']
+    return jsonify(contents)
+
+@app.route('/movies/id/<id>', methods=['GET'])
+def movie(id):
+    contents = urllib.request.urlopen("http://www.omdbapi.com/?i="+id+"&apikey=101cdcd1").read()
+    contents = json.loads(contents.decode('utf-8'))
     return jsonify(contents)
 
 if __name__ == '__main__':
