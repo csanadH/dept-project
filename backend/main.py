741d6f065a2ea5311501eb965f02a2d62a1b87c2
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-11-22 12:00:39 +0100

    Create endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import urllib.request
+import json
 
 DEBUG = True
 
@@ -7,6 +9,15 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 CORS(app)
+
+@app.route('/movies/<movie>', methods=['GET'])
+def movies(movie):
+    response_object = {'status': 'success'}
+    print(movie)
+    contents = urllib.request.urlopen("http://www.omdbapi.com/?s="+movie+"&apikey=101cdcd1").read()
+    contents = json.loads(contents.decode('utf-8'))
+    contents = contents['Search']
+    return jsonify(contents)
 
 if __name__ == '__main__':
     app.run()
